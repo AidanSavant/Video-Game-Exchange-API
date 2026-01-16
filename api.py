d21f8ac122fddc1355891b8cf96b460599623d96
aidan <aidansavantgeneral@gmail.com>
2026-01-16 13:28:29 -0700

    Hopefully works

---
+++ api.py
@@ -0,0 +1,162 @@
+from typing import Dict, Any
+
+from users import Users
+from user import User, Game
+from user_auth import UserAuth
+
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+app = FastAPI()
+bearer = HTTPBearer()
+
+users = Users()
+auth_service = UserAuth(users)
+
+def auth_middleware(
+    credentials: HTTPAuthorizationCredentials = Depends(bearer)
+) -> User:
+    jwt: str = credentials.credentials
+
+    try:
+        jwt_payload: Dict[str, Any] = auth_service.verify_jwt(jwt)
+
+        user: User | None = auth_service.users.get_user(jwt_payload["sub"])
+        if not user:
+            raise HTTPException(
+                status_code=401,
+                detail="Failed to auth user! User does not exist!"
+            )
+
+        return user
+
+    except Exception:
+        raise HTTPException(
+            status_code=401,
+            detail="Failed to auth user! Invalid JWT!"
+        )
+
+
+# === User API === #
+
+@app.post("/api/register")
+def register(reg_body: Dict[str, str]) -> Dict[str, bool]:
+    email: str = reg_body["email"]
+    if users.get_user(email):
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered!"
+        )
+
+    user: User = User(
+        name=reg_body["name"],
+        email=email,
+        password=reg_body["password"],
+        street_address=reg_body["street_address"],
+    )
+
+    users.add_user(user)
+
+    return { "successful" : True }
+
+@app.post("/api/login")
+def login(user: Dict[str, str]) -> Dict[str, str]:
+    try:
+        jwt: str = auth_service.auth(user["email"], user["password"])
+        return { "jwt" : jwt }
+
+    except ValueError as e:
+        detail: str = str(e)
+        raise HTTPException(status_code=401, detail=detail)
+
+@app.get("/api/debug/self")
+def get_self(authed_user: User = Depends(auth_middleware)) -> Dict[str, str | dict]:
+
+    return {
+        "name": authed_user.name,
+        "email": authed_user.email,
+        "street_address": authed_user.street_address,
+        "games" : {
+            name: game.to_dict()
+            for name, game in authed_user.games.items()
+        }
+    }
+
+@app.put("/api/self")
+def update_self(
+    update_body: Dict[str, str], 
+    user: User = Depends(auth_middleware)
+) -> Dict[str, bool]:
+    users.update_user(
+        email=user.email,
+        name=update_body.get("name"),
+        street_address=update_body.get("street_address")
+    )
+
+    return { "successful" : True }
+
+# === Game API === #
+
+@app.post("/api/games")
+def add_game(
+    game_body: Dict[str, str | int],
+    authed_user: User = Depends(auth_middleware)
+) -> Dict[str, bool]:
+    try:
+        game: Game = Game(
+            name=str(game_body["name"]),
+            publisher=str(game_body["publisher"]),
+            year=int(game_body["year"]),
+            platform=str(game_body["platform"]),
+            condition=str(game_body["condition"])
+        )
+
+        users.add_game(authed_user.email, game)
+
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+    return { "successful" : True }
+
+@app.get("/api/games/{game_name}")
+def get_game(
+    game_name: str,
+    authed_user: User = Depends(auth_middleware)
+) -> Dict[str, str | int]:
+    game: Game | None = authed_user.get_game(game_name)
+    if not game:
+        raise HTTPException(status_code=404, detail="Failed to find game!")
+
+    return game.to_dict()
+
+@app.put("/api/games/{game_name}")
+def update_game(
+    game_name: str,
+    update_body: dict[str, str], 
+    authed_user: User = Depends(auth_middleware)
+) -> Dict[str, bool]:
+    try:
+        users.update_game(
+            email=authed_user.email,
+            game_name=game_name,
+            new_name=update_body.get("name"),
+            condition=update_body.get("condition")
+        )
+
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+    return { "successful" : True }
+
+@app.delete("/api/games/{game_name}")
+def delete_game(
+    game_name: str, 
+    authed_user: User = Depends(auth_middleware)
+) -> Dict[str, bool]:
+    try:
+        users.delete_game(email=authed_user.email, game_name=game_name)
+
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+    return { "successful" : True }
---
+++ user_auth.py
@@ -0,0 +1,49 @@
+import jwt
+
+from user import User
+from users import Users
+
+from typing import Dict, Any
+from datetime import datetime, timedelta
+
+class UserAuth:
+    SECRET_KEY = "secret_jwt_key123321!"
+
+    def __init__(self, users: Users) -> None:
+        self.users: Users = users
+
+    def register(
+        self,
+        name: str,
+        email: str,
+        password: str,
+        street_address: str
+    ) -> None:
+        user = User(
+            name=name,
+            email=email,
+            password=password,
+            street_address=street_address,
+            games={}
+        )
+
+        self.users.add_user(user)
+
+    def auth(self, email: str, password: str) -> str:
+        user: User | None = self.users.get_user(email)
+        if not user or user.password != password:
+            raise ValueError("Failed to authenticate user! Invalid credentials!")
+
+        return self._create_jwt(user)
+
+    def _create_jwt(self, user: User) -> str:
+        payload = {
+            "sub": user.email,
+            "exp": datetime.utcnow() + timedelta(minutes=60)
+        }
+
+        return jwt.encode(payload, self.SECRET_KEY, algorithm="HS256")
+
+    def verify_jwt(self, token: str) -> Dict[str, Any]:
+        return jwt.decode(token, self.SECRET_KEY, algorithms=["HS256"])
+
